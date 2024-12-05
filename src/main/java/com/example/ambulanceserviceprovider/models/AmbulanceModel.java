fc7265ede01bcc871767bb34ec3e00057b7d2a4e
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-12-05 16:48:53 +0100

    Add getAmbulance() Method

--- src/main/java/com/example/ambulanceserviceprovider/models/AmbulanceModel.java
+++ src/main/java/com/example/ambulanceserviceprovider/models/AmbulanceModel.java
@@ -1,5 +1,26 @@
 package com.example.ambulanceserviceprovider.models;
 
+import com.example.ambulanceserviceprovider.datatypes.Ambulance;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AmbulanceModel {
-	
+	public Ambulance getAmbulance(int ambId) throws SQLException {
+		Ambulance ambulance = new Ambulance();
+
+		try (Connection con = DBModel.getDBConnection()) {
+			String statement = "SELECT * FROM ambulance WHERE amb_id = %d".formatted(ambId);
+			ResultSet result = con.prepareStatement(statement).executeQuery();
+
+			result.next();
+
+			ambulance.setId(result.getInt("amb_id"));
+			ambulance.setPlatenum(result.getString("amb_platenum"));
+			ambulance.setStatus(result.getInt("amb_status"));
+		}
+
+		return ambulance;
+	}
 }
