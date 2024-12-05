0a723ea44f9a634d99f6a1f5496d4b7597ee1444
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-12-05 12:42:41 +0100

    Add getAllUsers() and addUser() Method

--- src/main/java/com/example/ambulanceserviceprovider/models/UserModel.java
+++ src/main/java/com/example/ambulanceserviceprovider/models/UserModel.java
@@ -1,39 +1,70 @@
 package com.example.ambulanceserviceprovider.models;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserModel {
+	private List<String> usertype;
+
 	public User getUser(String email) throws SQLException {
 		User user;
-		String statement = "SELECT * from appuser WHERE email = %s".formatted(email);
 
 		try (Connection con = DBModel.getDBConnection()) {
+			String statement = "SELECT * from appuser WHERE user_email = '%s'".formatted(email);
 			ResultSet result = con.prepareStatement(statement).executeQuery();
 
 			user = new User();
 
-			user.setId(result.getInt("id"));
-			user.setName(result.getString("name"));
-			user.setEmail(result.getString("email"));
-			user.setPhoneNo(result.getString("phonenumber"));
-			user.setType(result.getString("type"));
-		} catch (SQLException e) {
-			throw e;
+			user.setId(result.getInt("user_id"));
+			user.setName(result.getString("user_name"));
+			user.setEmail(result.getString("user_email"));
+			user.setPhoneNum(result.getString("user_phonenum"));
+			user.setType(result.getInt("user_type"));
 		}
 
 		return user;
+	}
+
+	public List<User> getAllUsers() throws SQLException {
+		List<User> users = new ArrayList<>();
+
+		try (Connection con = DBModel.getDBConnection()) {
+			String statement = "SELECT * FROM appuser";
+			ResultSet userResult = con.prepareStatement(statement).executeQuery();
+
+			while (userResult.next()) {
+				User user = new User();
+				user.setId(userResult.getInt("user_id"));
+				user.setName(userResult.getString("user_name"));
+				user.setEmail(userResult.getString("user_email"));
+				user.setPhoneNum(userResult.getString("user_phonenum"));
+				user.setType(userResult.getInt("user_type"));
+
+				users.add(user);
+			}
+		}
+
+		return users;
+	}
+
+	public void addUser (String name, String email, String phoneno, int type) throws SQLException {
+		try (Connection con = DBModel.getDBConnection()) {
+			String statement = "INSERT INTO appuser (user_name, user_email, user_phonenum, user_type) VALUES (%s, %s, %s, %d)".formatted(name, email, phoneno, type);
+
+			con.prepareStatement(statement).execute();
+		}
 	}
 }
 
 class User {
 	private int id;
 	private String name;
-	private String phoneNo;
+	private String phoneNum;
 	private String email;
-	private String type;
+	private int type;
 
 	public int getId() {
 		return id;
@@ -43,15 +74,15 @@ class User {
 		return name;
 	}
 
-	public String getPhoneNo() {
-		return phoneNo;
+	public String getPhoneNum() {
+		return phoneNum;
 	}
 
 	public String getEmail() {
 		return email;
 	}
 
-	public String getType() {
+	public int getType() {
 		return type;
 	}
 
@@ -63,15 +94,15 @@ class User {
 		this.name = name;
 	}
 
-	public void setPhoneNo(String phoneNo) {
-		this.phoneNo = phoneNo;
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
 	}
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-	public void setType(String type) {
+	public void setType(int type) {
 		this.type = type;
 	}
 }
