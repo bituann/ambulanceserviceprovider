13b557492b5df30e6236dd0b910091095acc73a5
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-12-05 16:51:16 +0100

    Add addAmbulance() Method

--- src/main/java/com/example/ambulanceserviceprovider/models/AmbulanceModel.java
+++ src/main/java/com/example/ambulanceserviceprovider/models/AmbulanceModel.java
@@ -23,4 +23,12 @@ public class AmbulanceModel {
 
 		return ambulance;
 	}
+
+	public void addAmbulance (int id, String plateNum, int status) throws  SQLException {
+		try (Connection con = DBModel.getDBConnection()) {
+			String statement = "INSERT INTO ambulance (amb_id, amb_platenum, amb_status) VALUES (%d, %s, %d)".formatted(id, plateNum, status);
+
+			con.prepareStatement(statement).execute();
+		}
+	}
 }
