9ed078a9d8be570373ac1e9aefff29eb0b8ec65d
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-12-09 16:18:50 +0100

    Create Complete Emergency Datatype

---
+++ src/main/java/com/example/ambulanceserviceprovider/datatypes/Emergency.java
@@ -0,0 +1,61 @@
+package com.example.ambulanceserviceprovider.datatypes;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Emergency {
+	private int id;
+	private String title;
+	private String desc;
+	private LocalDate date;
+	private String location;
+	private LocalTime time;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
+}
