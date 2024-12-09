5d39e5a4fe416b4053f3d323c5db7b2d8381b4e4
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-12-09 16:04:43 +0100

    Add getAllUserNotification to Notification Model Class

--- src/main/java/com/example/ambulanceserviceprovider/models/NotificationModel.java
+++ src/main/java/com/example/ambulanceserviceprovider/models/NotificationModel.java
@@ -5,13 +5,16 @@ import com.example.ambulanceserviceprovider.datatypes.Notification;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotificationModel {
 	public Notification getNotification (int id) throws SQLException {
 		Notification notification = new Notification();
 
 		try (Connection con = DBModel.getDBConnection()) {
-			String statement = "SELECT * from notification where id=%d".formatted(id);
+			String statement = "SELECT * FROM notification WHERE notif_id = %d".formatted(id);
 			ResultSet result = con.prepareStatement(statement).executeQuery();
 
 			result.next();
@@ -22,5 +25,26 @@ public class NotificationModel {
 		}
 
 		return notification;
+	}
+
+	public List<Notification> getAllUserNotifications (int userId) throws SQLException {
+		List<Notification> notifications = new ArrayList<>();
+
+		try (Connection con = DBModel.getDBConnection()) {
+			String statement = "SELECT * FROM notification WHERE notif_userid = %d".formatted(userId);
+
+			ResultSet result = con.prepareStatement(statement).executeQuery();
+
+			while (result.next()) {
+				Notification notification = new Notification();
+				notification.setId(result.getInt("notif_id"));
+				notification.setUserId(result.getInt("notif_userid"));
+				notification.setTitle(result.getString("notif_title"));
+
+				notifications.add(notification);
+			}
+		}
+
+		return notifications;
 	}
 }
