f7e97d10b85efaaeaac730f74bd68deaa5bf01ea
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-12-09 17:03:13 +0100

    Add addEmergency Method to EmergencyModel Class

--- src/main/java/com/example/ambulanceserviceprovider/models/EmergencyModel.java
+++ src/main/java/com/example/ambulanceserviceprovider/models/EmergencyModel.java
@@ -3,8 +3,13 @@ package com.example.ambulanceserviceprovider.models;
 import com.example.ambulanceserviceprovider.datatypes.Emergency;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmergencyModel {
 	public Emergency getEmergency(int id) throws SQLException {
@@ -22,8 +27,47 @@ public class EmergencyModel {
 			emergency.setDate(result.getDate("emer_date").toLocalDate());
 			emergency.setLocation(result.getString("emer_desc"));
 			emergency.setTime(result.getTime("emer_timereported").toLocalTime());
+			emergency.setUserId(result.getInt("emer_userid"));
 		}
 
 		return emergency;
+	}
+
+	public List<Emergency> getAllEmergency () throws SQLException {
+		List<Emergency> emergencies = new ArrayList<>();
+
+		try (Connection con = DBModel.getDBConnection()) {
+			String statement = "SELECT * FROM emergency";
+			ResultSet result = con.prepareStatement(statement).executeQuery();
+
+			while (result.next()) {
+				Emergency emergency = new Emergency();
+
+				emergency.setId(result.getInt("emer_id"));
+				emergency.setTitle(result.getString("emer_title"));
+				emergency.setDesc(result.getString("emer_desc"));
+				emergency.setDate(result.getDate("emer_date").toLocalDate());
+				emergency.setLocation(result.getString("emer_desc"));
+				emergency.setTime(result.getTime("emer_timereported").toLocalTime());
+				emergency.setUserId(result.getInt("emer_userid"));
+
+				emergencies.add(emergency);
+			}
+		}
+
+		return emergencies;
+	}
+
+	public void addEmergency (String title, String desc, LocalDate date, String location, LocalTime time, int userId) throws SQLException {
+		try (Connection con = DBModel.getDBConnection()) {
+			String statement = "INSERT INTO emergency (emer_title, emer_desc, emer_date, emer_location, emer_time, emer_userid)" +
+					"VALUES (%s, %s, ?, %s, ?, %d)".formatted(title, desc, location, userId);
+
+			PreparedStatement preparedStatement = con.prepareStatement(statement);
+			preparedStatement.setObject(1, date);
+			preparedStatement.setObject(2, time);
+
+			preparedStatement.execute();
+		}
 	}
 }
