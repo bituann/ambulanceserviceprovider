b0029018e192eed888b51d8b71dc5fbb7ee71cc1
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-12-10 14:06:51 +0100

    Add Functionality For Register Button

--- src/main/java/com/example/ambulanceserviceprovider/controllers/SignInController.java
+++ src/main/java/com/example/ambulanceserviceprovider/controllers/SignInController.java
@@ -1,13 +1,21 @@
 package com.example.ambulanceserviceprovider.controllers;
 
+import com.example.ambulanceserviceprovider.HelloApplication;
 import com.example.ambulanceserviceprovider.datatypes.*;
 import com.example.ambulanceserviceprovider.models.UserModel;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Objects;
 
@@ -30,5 +38,15 @@ public class SignInController {
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public void clickRegisterButton (ActionEvent event) throws IOException {
+		Parent signUpParent = FXMLLoader.load(HelloApplication.class.getResource("signup.fxml"));
+		Scene signUpScene = new Scene(signUpParent);
+
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+		window.setScene(signUpScene);
+		window.show();
 	}
 }
