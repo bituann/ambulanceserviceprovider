1fc0bf449395fe8bf914493980b6e6beec8b03aa
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-12-10 16:02:32 +0100

    Add UserType Datatype Class

---
+++ src/main/java/com/example/ambulanceserviceprovider/datatypes/UserType.java
@@ -0,0 +1,22 @@
+package com.example.ambulanceserviceprovider.datatypes;
+
+public class UserType {
+	private int id;
+	private String userType;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+}
