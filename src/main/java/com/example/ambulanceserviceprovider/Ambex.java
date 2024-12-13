6c787e39ec17149cc7bd22979258b78b7a5ef019
Tobi Awanebi <113036994+bituann@users.noreply.github.com>
2024-12-13 14:22:41 +0100

    Rename File

--- src/main/java/com/example/ambulanceserviceprovider/Ambex.java
+++ src/main/java/com/example/ambulanceserviceprovider/Ambex.java
@@ -1,7 +1,5 @@
 package com.example.ambulanceserviceprovider;
 
-import com.example.ambulanceserviceprovider.controllers.SignInController;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Ambex extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signin.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(Ambex.class.getResource("signin.fxml"));
 		Scene scene = new Scene(fxmlLoader.load());
 		stage.setTitle("Ambex Ambulance Service");
 		stage.setScene(scene);
